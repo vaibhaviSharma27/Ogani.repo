af912268e3f063b9c9136f1f12be7626d25a145d
Vaibhavi <vaibhavisharma.sv2527@gmail.com>
2026-07-10 15:55:47 +0530

    jygigjk

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -158,12 +158,13 @@ const Profile = () => {
               My Profile
             </button>
 
-
+          <Link to="/orders">
             <button onClick = {() => {navigator("/orders")}} className="w-full flex items-center gap-3 px-4 py-3 rounded-xl hover:bg-gray-100 transition text-gray-700">
               <ShoppingBag size={18} />
               My Orders
             
             </button>
+            </Link>
 
 
             <button onClick = {() => {navigator("/wishlist")}}
